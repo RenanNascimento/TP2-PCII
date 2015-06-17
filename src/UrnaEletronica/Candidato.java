d79f4df91b749f28e300d57df0c35f61bea50f58
RenanNascimento <renanmateusbn@gmail.com>
2015-06-16 22:41:47 -0300

    Comeco do programa feito no laboratorio

--- src/UrnaEletronica/Candidato.java
+++ src/UrnaEletronica/Candidato.java
@@ -1,5 +1,48 @@
 package UrnaEletronica;
 
-public class Candidato {
-  //
+public class Candidato 
+{
+    private String nome;
+    private String email;
+    private String dataNascimento;
+    private int codigo;
+
+    public Candidato(String nome, String email, String dataNascimento, int codigo) {
+        this.nome = nome;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
 }
