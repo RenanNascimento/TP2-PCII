2e828d0385dc2cd2f6e66363992ada8416e20441
RenanNascimento <renanmateusbn@gmail.com>
2015-06-19 10:41:54 -0300

    Deletada main e criada UrnaEletronica

--- src/UrnaEletronica/UrnaEletronica.java
+++ src/UrnaEletronica/UrnaEletronica.java
@@ -1,16 +1,15 @@
 package UrnaEletronica;
 
-import java.util.Scanner;
-
 import static UrnaEletronica.MenuCandidato.CandidatoMenu;
 
-public class Main {
+public class UrnaEletronica {
 
 	public static void main(String[] args) {
 		
 		CandidatoMenu();
-    }
-        
+
+	}
+
 }
 
-// Eu sou o SS
+
